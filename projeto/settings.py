cde29401aa51cc31e95e4b9f913ae84cea1ddfd3
João Pedro Calaça Costa <jotao22gm@gmail.com>
2025-08-02 18:06:21 -0300

    adiciona estrutura básica de templates e partes do layout para o projeto de receitas

--- projeto/settings.py
+++ projeto/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'base_templates',  # Directory for custom templates
+            BASE_DIR / 'base_templates',  # Directory for custom templates,
         ],
         'APP_DIRS': True,
         'OPTIONS': {
