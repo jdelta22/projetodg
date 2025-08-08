9db21942e74ce8abba607889da896bae7b0b4fa0
João Pedro Calaça Costa <jotao22gm@gmail.com>
2025-08-07 23:27:13 -0300

    terminando aula de templates

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -13,6 +13,7 @@ fake = Faker('pt_BR')
 
 def make_recipe():
     return {
+        'id': fake.random_number(digits=3, fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
@@ -29,7 +30,7 @@ def make_recipe():
             'name': fake.word()
         },
         'cover': {
-            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
+            'url': 'https://picsum.photos/%s/%s' % rand_ratio(),
         }
     }
 
