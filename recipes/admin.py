69c1772b2166b1b90ef54867d6729e857b087f99
João Pedro Calaça Costa <jotao22gm@gmail.com>
2025-08-18 20:14:25 -0300

    pre aula de testes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Category
+from .models import Recipe
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+admin.site.register(Category, CategoryAdmin)
