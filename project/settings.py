fb3cf12723db661a655f8eb7846c15991617bbdd
jdelta22 <jotao22gm@gmail.com>
2025-10-25 16:46:46 -0300

    config secrete key, debug and allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j@$88t09$hs!l#k_3hyqili9^9)y21r=+0m!!e)g8+wz%cq)c&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = list[str]= ['*']
 
 
 # Application definition
