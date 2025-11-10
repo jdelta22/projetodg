18d9b690b30a658d586ecaed2bc76b9b889c98cc
jdelta22 <jotao22gm@gmail.com>
2025-11-09 23:06:55 -0300

    DJANGO DEBUG TOOLL

--- project/settings.py
+++ project/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Custom apps
+    'debug_toolbar',
     'recipes',
     'authors',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,3 +148,8 @@ MESSAGE_TAGS = {
     constants.WARNING: 'message-warning',
     constants.ERROR: 'message-error',
 }
+
+# Debug Toolbar settings
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- recipes/views/RecipeListView.py
+++ recipes/views/RecipeListView.py
@@ -17,6 +17,7 @@ class RecipeListView(ListView):
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(is_published=True)
+        qs = qs.select_related('author', 'category')
         return qs 
 
     def get_context_data(self, *args, **kwargs):
